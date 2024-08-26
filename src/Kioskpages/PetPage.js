33022d67a18925e0bbd5a8250b469ce20f8887fe
Sutaenghhh <neocop7@gmail.com>
2024-08-26 14:19:57 +0900

    Your commit message

--- src/Kioskpages/PetPage.js
+++ src/Kioskpages/PetPage.js
@@ -1,20 +1,22 @@
-import React, { useContext, useState, useEffect } from 'react';
-import styled from 'styled-components';
+import React, { useContext, useState, useEffect, useMemo, useCallback } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { UserContext } from '../context/UserContext';
 import axios from 'axios';
 import { FaRegTrashAlt } from "react-icons/fa";
 import './PetPage.css';
 import QRCodeScanner from '../Kioskcomponents/QRCodeScanner';
-import { API_BASE_URL } from '../service/PetapiService'; // API_BASE_URL import
+import Swal from 'sweetalert2';
+import { debounce } from 'lodash';
+import AutoCapture from '../Kioskcomponents/AutoCapture'; // AutoCapture 임포트
 
 const PetPage = () => {
   const { items, setItems, allCart, setAllCart } = useContext(UserContext);
   const [totalPrice, setTotalPrice] = useState(0);
   const navigate = useNavigate();
   const [pendingUpdates, setPendingUpdates] = useState([]);
+  const [videoStream, setVideoStream] = useState(null);
 
-  const getSessionId = () => {
+  const getSessionId = useCallback(() => {
     if (typeof window !== 'undefined') {
       let sessionId = localStorage.getItem("sessionId");
       if (!sessionId) {
@@ -24,7 +26,7 @@ const PetPage = () => {
       return sessionId;
     }
     return null;
-  };
+  }, []);
 
   const generateUUID = () => {
     return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
@@ -33,11 +35,11 @@ const PetPage = () => {
     });
   };
 
-  const fetchCartList = async () => {
+  const fetchCartList = useCallback(async () => {
     try {
-      const sessionId = getSessionId(); // 고유 식별자 가져오기
-      const response = await axios.get('/petShop/cart/cartList', { 
-        params: { sessionId } // 고유 식별자를 서버로 전달
+      const sessionId = getSessionId();
+      const response = await axios.get('/petShop/cart/cartList', {
+        params: { sessionId }
       });
 
       if (response.status !== 200) {
@@ -49,44 +51,71 @@ const PetPage = () => {
     } catch (error) {
       console.error('Error fetching data:', error);
     }
-  };
+  }, [getSessionId, setAllCart, setItems]);
 
   useEffect(() => {
-    console.log(sessionId);
     fetchCartList();
-  }, [setItems, setAllCart]);
+  }, [fetchCartList]);
 
-  useEffect(() => {
-    const total = items.reduce((sum, item) => sum + item.pdPrice * item.cartCount, 0);
-    setTotalPrice(total);
+  const totalPriceMemo = useMemo(() => {
+    return items.reduce((sum, item) => sum + item.pdPrice * item.cartCount, 0);
   }, [items]);
 
-  const handleRemoveItem = async (pdIdx) => {
-    const sessionId = getSessionId(); // 세션 ID를 가져옵니다.
+  useEffect(() => {
+    setTotalPrice(totalPriceMemo);
+  }, [totalPriceMemo]);
 
-    // 클라이언트 상태에서 아이템을 먼저 제거
+  const debouncedUpdateCartOnServer = useCallback(
+    debounce((sessionId, pdIdx, cartCount) => {
+      axios.put('/petShop/cart/updateCart', [
+        {
+          sessionId,
+          pdIdx,
+          cartCount
+        }
+      ]).catch(error => {
+        console.error('Error updating cart on server:', error);
+        Swal.fire({
+          title: '수량 변경 오류',
+          text: '수량 변경에 실패했습니다. 다시 시도해 주세요.',
+          icon: 'error',
+          confirmButtonText: '확인'
+        });
+      });
+    }, 300), []
+  );
+
+  const handleRemoveItem = useCallback(async (pdIdx) => {
+    const sessionId = getSessionId();
+
     const updatedItems = items.filter(item => item.pdIdx !== pdIdx);
     setAllCart(updatedItems);
     setItems(updatedItems);
 
-    // 대기 중인 업데이트 목록에서 해당 항목 제거
     setPendingUpdates(prev => prev.filter(update => update.pdIdx !== pdIdx));
 
     try {
-      // 서버에 삭제 요청
-      await axios.delete('api/petShop/cart/deleteCartItem', {
-        params: { sessionId, pdIdx }  // sessionId와 pdIdx를 서버에 전달
+      await axios.delete('/petShop/cart/deleteCartItem', {
+        params: { sessionId, pdIdx }
       });
-
-      console.log(`Item with pdIdx ${pdIdx} has been removed from the server.`);
+      Swal.fire({
+        title: '상품 삭제',
+        text: '상품 삭제에 성공했습니다.',
+        icon: 'success',
+        confirmButtonText: '확인'
+      });
     } catch (error) {
       console.error('Error removing item from the server:', error);
-      alert('서버에서 항목을 삭제하는 중 오류가 발생했습니다. 다시 시도해 주세요.');
+      Swal.fire({
+        title: '수량 삭제 오류',
+        text: '수량 삭제에 실패했습니다. 다시 시도해 주세요.',
+        icon: 'error',
+        confirmButtonText: '확인'
+      });
     }
-  };
+  }, [getSessionId, items]);
 
-  const handlecartCountChange = (pdIdx, delta) => {
-    // 클라이언트 상태 즉시 업데이트
+  const handlecartCountChange = useCallback((pdIdx, delta) => {
     const updatedItems = items.map(item =>
       item.pdIdx === pdIdx ? { ...item, cartCount: Math.max(1, item.cartCount + delta) } : item
     );
@@ -106,134 +135,118 @@ const PetPage = () => {
         return [...prev, { sessionId: getSessionId(), pdIdx, cartCount: updatedItem.cartCount }];
       });
 
-      // 서버에 비동기로 업데이트 요청을 보냅니다.
-      axios.put('api/petShop/cart/updateCart', [
-        {
-          sessionId: getSessionId(),
-          pdIdx: updatedItem.pdIdx,
-          cartCount: updatedItem.cartCount
-        }
-      ])
-        .catch(error => {
-          console.error('Error updating cart on server:', error);
-          alert('장바구니를 업데이트하는 중 오류가 발생했습니다. 다시 시도해 주세요.');
-        });
+      debouncedUpdateCartOnServer(getSessionId(), pdIdx, updatedItem.cartCount);
     }
-  };
+  }, [items, debouncedUpdateCartOnServer, getSessionId]);
 
-  const handlePayment = async () => {
+  const handlePayment = useCallback(async () => {
     if (pendingUpdates.length > 0) {
       try {
-        await axios.put('api/petShop/cart/updateCart', pendingUpdates);
-        setPendingUpdates([]); // Clear the pending updates after successful update
+        await axios.put('/petShop/cart/updateCart', pendingUpdates);
+        setPendingUpdates([]);
         navigate('/payment/checkout');
       } catch (error) {
         console.error('Error updating cart on server:', error);
-        alert('장바구니를 업데이트하는 중 오류가 발생했습니다. 다시 시도해 주세요.');
+        Swal.fire({
+          title: '장바구니 업데이트 오류',
+          text: '장바구니 업데이트에 실패했습니다. 다시 시도해 주세요.',
+          icon: 'error',
+          confirmButtonText: '확인'
+        });
       }
     } else {
       navigate('/payment/checkout');
     }
+  }, [pendingUpdates, navigate]);
+
+  const handleCapture = async (imageData) => {
+    localStorage.setItem('image', imageData); // 로컬 스토리지에 저장
+    console.log('Captured face image:', imageData);
+
+    try {
+      // 얼굴 이미지를 서버로 전송
+      await axios.post('/petShop/cart/customerFaceCompare', { image: imageData });
+     
+    } catch (error) {
+      console.error('Error sending face image:', error);
+     
+    }
   };
 
-  const sessionId = getSessionId();
-
-    // 랜덤 아이템 추가 함수
-    const handleAddRandomItem = () => {
-      const randomIndex = Math.floor(Math.random() * 100); // 예시로 1~100까지의 인덱스
-      const randomPrice = Math.floor(Math.random() * 4000) + 1; // 1~4000 사이의 랜덤 가격
-      const newItem = {
-        pdIdx: `random-${randomIndex}`, // 고유 인덱스
-        pdName: `상품${randomIndex + 1}`, // 상품 이름
-        pdPrice: randomPrice,
-        cartCount: 1 // 기본 수량 1로 설정
-      };
-  
-      // 상태 업데이트
-      const updatedItems = [...items, newItem];
-      setAllCart(updatedItems);
-      setItems(updatedItems);
+  useEffect(() => {
+    // 비디오 스트림을 설정하는 로직
+    const startVideoStream = async () => {
+      try {
+        const stream = await navigator.mediaDevices.getUserMedia({ video: true });
+        setVideoStream(stream);
+      } catch (error) {
+        console.error('Error accessing webcam:', error);
+        Swal.fire({
+          title: '웹캠 접근 오류',
+          text: '웹캠에 접근할 수 없습니다. 웹캠이 연결되어 있는지 확인해 주세요.',
+          icon: 'error',
+          confirmButtonText: '확인'
+        });
+      }
     };
 
+    startVideoStream();
+
+    return () => {
+      if (videoStream) {
+        const tracks = videoStream.getTracks();
+        tracks.forEach(track => track.stop());
+      }
+    };
+  }, []);
+
   return (
-    <Container>
-      <TopHalf className="Top-Main">
-        <QRCodeScanner sessionId={sessionId} onCartUpdated={fetchCartList} />
-      </TopHalf>
-      <BottomHalf className="Bottom-Main">
+    <div className="PetPage-Container">
+      <div className="PetPage-TopHalf">
+        <QRCodeScanner sessionId={getSessionId()} onCartUpdated={fetchCartList} />
+        <AutoCapture 
+          videoStream={videoStream} 
+          onCapture={handleCapture} // 자동 캡처 후 처리할 함수
+        />
+      </div>
+      <div className="PetPage-BottomHalf">
         <div className="table-container">
           <table className="data-table">
             <tbody>
-              {allCart.map(item => (
-                <tr key={item.pdIdx} className="tr_table">
-                  <td>{item.pdName}</td>
-                  <td className="kiosk-button">
-                    <button
-                      onClick={() => handlecartCountChange(item.pdIdx, -1)}
-                      className="Kiosk-cartCount-button"
-                    ><span className="minus-sign">-</span></button>
-                    {item.cartCount}
-                    <button
-                      onClick={() => handlecartCountChange(item.pdIdx, 1)}
-                      className="Kiosk-cartCount-button"
-                    >+</button>
-                  </td>
-                  <td>{(item.pdPrice * item.cartCount).toLocaleString()}원</td>
-                  <td>
-                    <button onClick={() => handleRemoveItem(item.pdIdx)} className='Trash_Button'>
-                      <FaRegTrashAlt />
-                    </button>
-                  </td>
+              {allCart.length > 0 ? (
+                allCart.map(item => (
+                  <tr key={item.pdIdx}>
+                    <td>{item.pdName}</td>
+                    <td className="kiosk-button">
+                      <button
+                        onClick={() => handlecartCountChange(item.pdIdx, -1)}
+                        className="cartCount-button"
+                      ><span className="minus-sign">-</span></button>
+                      {item.cartCount}
+                      <button
+                        onClick={() => handlecartCountChange(item.pdIdx, 1)}
+                        className="Kiosk-cartCount-button"
+                      >+</button>
+                    </td>
+                    <td>{(item.pdPrice * item.cartCount).toLocaleString()}원</td>
+                    <td><button onClick={() => handleRemoveItem(item.pdIdx)} className='Trash_Button'><FaRegTrashAlt /></button></td>
+                  </tr>
+                ))
+              ) : (
+                <tr>
+                  <td colSpan="4">장바구니가 비어 있습니다.</td>
                 </tr>
-              ))}
+              )}
             </tbody>
           </table>
         </div>
         <div className="summary">
           <p className='Pet-total'>총 금액: {totalPrice.toLocaleString()}원</p>
         </div>
-        <button className="PetPage_button" onClick={handleAddRandomItem}>
-          랜덤 아이템 추가
-        </button>
-        <button className="PetPage_button" onClick={handlePayment} disabled={totalPrice <= 0}>
-          결제하기
-        </button>
-      </BottomHalf>
-      <div className="petPage-center-text-bar">
-        상품의 QR코드를 스캔 해 주세요
+        <button className="payment-button" onClick={handlePayment}>결제하기</button>
       </div>
-    </Container>
+    </div>
   );
 };
 
 export default PetPage;
-
-// Styled components
-const Container = styled.div`
-  display: flex;
-  flex-direction: column;
-  height: 100vh;
-  position: relative;
-`;
-
-const TopHalf = styled.div`
-  width: 100%;
-  height: 50%;
-  background-color: #FFFFFF; /* 위쪽 절반의 배경색 */
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  position: relative;
-`;
-
-const BottomHalf = styled.div`
-  width: 100%;
-  height: 50%;
-  background-color: #FFECA0; /* 아래쪽 절반의 배경색 */
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  position: relative;
-`;
